f7aeb837f36369327d48344e5b463d9b268d884a
Kalen Arendt <dev.KalenArendt@Gmail.com>
2024-02-23 17:03:57 -0600

    [main]: initial commit

---
+++ HANKer/Assets/LineCollider.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+[RequireComponent(typeof(PolygonCollider2D))]
+public class LineCollider : MonoBehaviour
+{
+
+   private void Awake ()
+   {
+      var line = GetComponent<LineRenderer>();
+      var poly = GetComponent<PolygonCollider2D>();
+
+      var points = new Vector3[line.positionCount];
+      line.GetPositions(points);
+
+      poly.points = points.Select(x => (Vector2)x).ToArray();
+   }
+
+
+   // Start is called before the first frame update
+   void Start ()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update ()
+   {
+
+   }
+}
