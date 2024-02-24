b47fe2cfe03b5d0d9e9d1e3e5c13a9eedb3c6067
Kalen Arendt <dev.KalenArendt@Gmail.com>
2024-02-24 14:57:00 -0600

    added particle

--- HANKer/Assets/Scripts/PlayerHandUI.cs
+++ HANKer/Assets/Scripts/PlayerHandUI.cs
@@ -1,9 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
    public class PlayerHandUI : MonoBehaviour
    {
+      [SerializeField] List<CardSlotUI> slots = new List<CardSlotUI>();
+      
 
+      
+   }
+
+
+   public class CardSlotUI : MonoBehaviour
+   {
+      [SerializeField] Image image;
+      [SerializeField] CardsConfig config;
+
+
+
+      void Set(Card card)
+      {
+         image.sprite = config.GetSpriteFromCardType(card.Type);
+         image.color = config.GetColorFromCardColor(card.Color);
+      }
    }
 }
